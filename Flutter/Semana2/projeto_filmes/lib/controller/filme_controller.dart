9156c551082b6ca8e380dadd4b5402aab822b626
Pedro Henrique <pedibr5@gmail.com>
2025-04-29 17:01:02 -0300

    29.04.2025

--- Flutter/Semana2/projeto_filmes/lib/controller/filme_controller.dart
+++ Flutter/Semana2/projeto_filmes/lib/controller/filme_controller.dart
@@ -5,9 +5,10 @@ import 'package:http/http.dart' as http;
 import '../model/filme.dart';
 
 class FilmeController{
+  final String apiKey = '';
   Future<List<Filme>> getFilmes() async {
     final response = await http.get(
-      Uri.parse('https://api.themoviedb.org/3/discover/movie?include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key='),
+      Uri.parse('https://api.themoviedb.org/3/discover/movie?include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=$apiKey'),
     );
 
     if (response.statusCode == 200) {
@@ -21,5 +22,20 @@ class FilmeController{
     }
   }
 
+  Future<List<Filme>> getMostRated() async{
+    final currentYear = DateTime.now().year;
+    final response = await http.get(
+      Uri.parse('https://api.themoviedb.org/3/discover/movie?include_video=false&language=en-US&page=1&sort_by=vote_average.desc&primary_release_year=$currentYear&api_key=$apiKey'),
+    );
 
+    if (response.statusCode == 200) {
+      print(jsonDecode(response.body) as Map<String, dynamic>);
+      final Map<String, dynamic> data = jsonDecode(response.body) as Map<String, dynamic>;
+      final List<dynamic> results = data['results'] as List<dynamic>;
+      return results.map((filmeJson) => Filme.fromJson(filmeJson as Map<String, dynamic>)).toList();
+    }
+    else {
+      throw Exception("Falha ao carregar filme!");
+    }
+  }
 }
--- Flutter/Semana2/projeto_filmes/lib/views/home_page.dart
+++ Flutter/Semana2/projeto_filmes/lib/views/home_page.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:projeto_filmes/controller/filme_controller.dart';
 import 'package:projeto_filmes/model/filme.dart';
-import 'package:projeto_filmes/widgets/curved_container.dart';
+import 'package:projeto_filmes/theme/app_colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,94 +13,284 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Future<List<Filme>> futureFilmes;
+  late Future<List<Filme>> mostRatedFilmes;
   FilmeController filmeController = FilmeController();
+  final CarouselSliderController _posterController = CarouselSliderController();
+  final CarouselSliderController _titleController = CarouselSliderController();
 
   @override
   void initState() {
     super.initState();
     futureFilmes = filmeController.getFilmes();
+    mostRatedFilmes = filmeController.getMostRated();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: () {
-            Navigator.pushNamed(context, '/search');
-          }, icon: Icon(Icons.search, color: Colors.white,))
-        ],
-        centerTitle: true,
-        title: Text("Absolute Cinema", style: TextStyle(color: Colors.white70)),
-        backgroundColor: Color(0xFF6B0000),
-      ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            CurvedContainer(
-              curveHeight: 75,
-              color: Colors.white70,
-              child: Column(
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.57,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                alignment: Alignment.bottomLeft,
                 children: [
-                  SizedBox(height: 24),
-                  Text(
-                    "Populares Agora!",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
-                  ),
+                  Positioned(
+                    top: 0,
+                    child: Material(
+                      elevation: 20,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: FutureBuilder<List<Filme>>(
+                          future: futureFilmes,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              final List<String?> posterPaths =
+                                  snapshot.data!
+                                      .map<String?>((filme) => filme.posterPath)
+                                      .toList();
 
-                  SizedBox(height: 24),
-                  FutureBuilder<List<Filme>>(
-                    future: futureFilmes,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        final List<String?> posterPaths =
-                            snapshot.data!
-                                .map<String?>((filme) => filme.posterPath)
-                                .toList();
-
-                        return CarouselSlider(
-                          items:
-                              posterPaths.map((posterPath) {
-                                return Builder(
-                                  builder: (BuildContext context) {
-                                    return Container(
-                                      width:
-                                          MediaQuery.of(context).size.width *
-                                          0.3,
-                                      child: Image.network(
-                                        'https://image.tmdb.org/t/p/w500$posterPath',
-                                        fit: BoxFit.contain,
-                                      ),
+                              return CarouselSlider(
+                                carouselController: _posterController,
+                                items:
+                                    posterPaths.map((posterPath) {
+                                      return Builder(
+                                        builder: (BuildContext context) {
+                                          return Container(
+                                            color: Colors.black,
+                                            child: Image.network(
+                                              'https://image.tmdb.org/t/p/w500$posterPath',
+                                              width:
+                                                  MediaQuery.of(
+                                                    context,
+                                                  ).size.width,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          );
+                                        },
+                                      );
+                                    }).toList(),
+                                options: CarouselOptions(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.5,
+                                  aspectRatio: 16 / 9,
+                                  viewportFraction: 1,
+                                  initialPage: 0,
+                                  enableInfiniteScroll: false,
+                                  reverse: false,
+                                  autoPlay: true,
+                                  autoPlayInterval: Duration(seconds: 3),
+                                  autoPlayAnimationDuration: Duration(
+                                    milliseconds: 800,
+                                  ),
+                                  autoPlayCurve: Curves.fastOutSlowIn,
+                                  scrollDirection: Axis.horizontal,
+                                  onPageChanged: (index, reason) {
+                                    _titleController.animateToPage(
+                                      index,
+                                      duration: Duration(milliseconds: 300),
+                                      curve: Curves.easeInOut,
                                     );
                                   },
-                                );
-                              }).toList(),
-                          options: CarouselOptions(
-                            height: 400,
-                            aspectRatio: 16 / 9,
-                            viewportFraction: 0.3,
-                            initialPage: 0,
-                            enableInfiniteScroll: false,
-                            reverse: false,
-                            autoPlay: true,
-                            autoPlayInterval: Duration(seconds: 3),
-                            autoPlayAnimationDuration: Duration(
-                              milliseconds: 800,
-                            ),
-                            autoPlayCurve: Curves.fastOutSlowIn,
-                            enlargeCenterPage: true,
-                            enlargeFactor: 0.3,
-                            scrollDirection: Axis.horizontal,
-                          ),
-                        );
-                      } else if (snapshot.hasError) {
-                        return Text('${snapshot.error}');
-                      }
+                                ),
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text('${snapshot.error}');
+                            }
 
-                      return const CircularProgressIndicator();
-                    },
+                            return const CircularProgressIndicator();
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 40,
+                    child: Material(
+                      elevation: 10,
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(50),
+                        topRight: Radius.circular(50),
+                      ),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.secondary,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(50),
+                            bottomRight: Radius.circular(50),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Destaques Recentes:",
+                                style: Theme.of(context).textTheme.headlineLarge
+                                    ?.copyWith(color: AppColors.primary600),
+                              ),
+                              SizedBox(height: 8),
+                              SizedBox(
+                                height: 24,
+                                child: FutureBuilder<List<Filme>>(
+                                  future: futureFilmes,
+                                  builder: (context, snapshot) {
+                                    if (snapshot.hasData) {
+                                      final List<String?> movieTitles =
+                                          snapshot.data!
+                                              .map<String?>(
+                                                (filme) => filme.title,
+                                              )
+                                              .toList();
+                                      return CarouselSlider(
+                                        carouselController: _titleController,
+
+                                        items:
+                                            movieTitles.map((title) {
+                                              return Builder(
+                                                builder: (
+                                                  BuildContext context,
+                                                ) {
+                                                  return Text(
+                                                    title ?? 'Sem Título',
+                                                    style: Theme.of(context)
+                                                        .textTheme
+                                                        .headlineLarge
+                                                        ?.copyWith(
+                                                          color:
+                                                              AppColors
+                                                                  .primary600,
+                                                        ),
+                                                    textAlign: TextAlign.start,
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                  );
+                                                },
+                                              );
+                                            }).toList(),
+                                        options: CarouselOptions(
+                                          height: 24,
+                                          viewportFraction: 1,
+                                          initialPage: 0,
+                                          enableInfiniteScroll: true,
+                                          reverse: false,
+                                          autoPlay: true,
+                                          autoPlayInterval: Duration(
+                                            seconds: 3,
+                                          ),
+                                          autoPlayAnimationDuration: Duration(
+                                            milliseconds: 500,
+                                          ),
+                                          autoPlayCurve: Curves.linear,
+                                          scrollDirection: Axis.horizontal,
+                                          onPageChanged: (index, reason) {
+                                            _posterController.animateToPage(
+                                              index,
+                                              duration: Duration(
+                                                milliseconds: 300,
+                                              ),
+                                              curve: Curves.easeInOut,
+                                            );
+                                          },
+                                        ),
+                                      );
+                                    } else if (snapshot.hasError) {
+                                      return Text("Erro ao carregar títulos");
+                                    }
+                                    return const SizedBox(
+                                      width: 24,
+                                      height: 24,
+                                      child: CircularProgressIndicator(),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
+              ),
+            ),
+            Text(
+              "Descubra seu Próximo Favorito",
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                color: AppColors.complementary300,
+                fontSize: 26,
+              ),
+            ),
+            SizedBox(height: 12),
+
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    AppColors.complementary300,
+                    AppColors.complementary200,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight
+                ),
+              ),
+              child: FutureBuilder<List<Filme>>(
+                future: mostRatedFilmes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final List<String?> posterPaths =
+                    snapshot.data!
+                        .map<String?>((filme) => filme.posterPath)
+                        .toList();
+
+                    return CarouselSlider(
+                      items:
+                      posterPaths.map((posterPath) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              child: Image.network(
+                                'https://image.tmdb.org/t/p/w500$posterPath',
+                                width:
+                                MediaQuery.of(
+                                  context,
+                                ).size.width,
+                                fit: BoxFit.cover,
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                      options: CarouselOptions(
+                        height: 150,
+                        viewportFraction: 0.3,
+                        initialPage: 0,
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: true,
+                        autoPlayInterval: Duration(seconds: 3),
+                        autoPlayAnimationDuration: Duration(
+                          milliseconds: 800,
+                        ),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        scrollDirection: Axis.horizontal,
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+
+                  return const CircularProgressIndicator();
+                },
               ),
             ),
           ],
